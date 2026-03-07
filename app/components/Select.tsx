4e3614eee1e913f5cabc8bf1d30c7e60ec7dd93a
IonutSaft <ionut.saft14@gmail.com>
2026-03-07 12:33:39 +0200

    Updated the color of some text

--- app/components/Select.tsx
+++ app/components/Select.tsx
@@ -25,7 +25,7 @@ export function Select({
       <select
         className={`
           px-3 py-2 border rounded-lg
-          text-sm font-medium
+          text-sm font-medium text-black
           focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent
           ${error ? "border-red-500 bg-red-50" : "border-gray-300 bg-white"}
           ${className}
